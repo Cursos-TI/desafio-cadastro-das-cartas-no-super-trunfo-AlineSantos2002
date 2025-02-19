b98999798ee38c84ddb68cedb42d81463c36b92b
AlineSantos2002 <alinevitoria.contato@gmail.com>
2025-02-19 14:51:57 +0000

    Finalizando

--- jogosupertrunfo.c
+++ jogosupertrunfo.c
@@ -4,13 +4,14 @@ int main(){
 
     printf("Desafio Super Trunfo!\n");
    // Declarando variáveis
-   char letra;
-   char codigo [3];
-   char nome da cidade [8];
-   int população;
-   float área;
-   float PIB;
-   int numero de pontos turisticos;
+   int carta = 2;
+   char estado [1] = 'B';
+   char codigo [3] = "B02"
+   char nome da cidade [30] = "E de Janeiro";
+   int população = 6748000;
+   float área = 1200.25;
+   float PIB = 300.5000,00;
+   int numero pontos turisticos = 50;
 
 
 }
