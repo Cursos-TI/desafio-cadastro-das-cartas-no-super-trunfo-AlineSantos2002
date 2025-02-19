b5260d67fccd8ec7524ba6faef8e4032aec48c31
AlineSantos2002 <alinevitoria.contato@gmail.com>
2025-02-19 14:27:55 +0000

    Inserido variaveis

---
+++ jogosupertrunfo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Desafio Super Trunfo!\n");
+   // Declarando variáveis
+   char letra;
+   char codigo [3];
+   char nome da cidade [8];
+   int população;
+   float área;
+   float PIB;
+   int numero de pontos turisticos;
+
+
+}
