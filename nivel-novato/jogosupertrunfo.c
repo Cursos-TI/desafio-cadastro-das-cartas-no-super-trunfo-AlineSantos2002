daf400019de552d0b3a05c34aafdb96794652bee
AlineSantos2002 <alinevitoria.contato@gmail.com>
2025-06-08 16:45:47 +0000

    Cadastrado 2 cartas

--- nivel-novato/jogosupertrunfo.c
+++ nivel-novato/jogosupertrunfo.c
@@ -1,17 +1,45 @@
 #include <stdio.h>
 
-int main(){
-
+int main() {
     printf("Desafio Super Trunfo!\n");
-   // Declarando variáveis
-   int carta = 2;
-   char estado [1] = 'B';
-   char codigo [3] = "B02"
-   char nome da cidade [30] = "E de Janeiro";
-   int população = 6748000;
-   float área = 1200.25;
-   float PIB = 300.5000,00;
-   int numero pontos turisticos = 50;
 
+    // Carta 1
+    char estado1 = 'A';
+    char codigo1[4] = "A01";
+    char nomeDaCidade1[30] = "SaoPaulo";
+    int populacao1 = 12350000;
+    float area1 = 1521.11;
+    float pib1 = 869.28;
+    int pontosTuristicos1 = 50;
 
+    // Carta 2
+    char estado2 = 'B';
+    char codigo2[4] = "B02";
+    char nomeDaCidade2[30] = "RioDeJaneiro";
+    int populacao2 = 6748000;
+    float area2 = 1200.25;
+    float pib2 = 300.5;
+    int pontosTuristicos2 = 50;
+
+    // Exibindo dados da Carta 1
+    printf("\n--- CARTA 1 ---\n");
+    printf("Estado: %c\n", estado1);
+    printf("Código: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeDaCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %.2f km²\n", area1);
+    printf("PIB: %.2f bilhões de reais\n", pib1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+
+    // Exibindo dados da Carta 2
+    printf("\n--- CARTA 2 ---\n");
+    printf("Estado: %c\n", estado2);
+    printf("Código: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeDaCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %.2f km²\n", area2);
+    printf("PIB: %.2f bilhões de reais\n", pib2);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+
+    return 0;
 }
